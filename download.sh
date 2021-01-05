2126b44d6dc9f8f68963687577106ee5f4e91342
Simeon Schüz <simeon.schuez@uni-jena.de>
2021-01-05 11:43:45 +0100

    moved data bash file + added extracting tar files

--- download.sh
+++ download.sh
@@ -1,3 +1,10 @@
 wget http://www.cs.toronto.edu/~faghri/vsepp/vocab.tar
 wget http://www.cs.toronto.edu/~faghri/vsepp/data.tar
 wget http://www.cs.toronto.edu/~faghri/vsepp/runs.tar
+
+tar xopf data.tar
+rm data.tar
+tar xopf runs.tar
+rm runs.tar
+tar xopf vocab.tar
+rm vocab.tar
