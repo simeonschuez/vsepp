4fb5d1c64e1e9d7ac78f6ea64908385891a3a2e2
Simeon Schüz <simeon.schuez@uni-jena.de>
2021-02-18 15:11:50 +0100

    bash files for batch eval

---
+++ batch_evaluate.sh
@@ -0,0 +1,1 @@
+python batch_evaluate.py --input_dir caps_files/test/ --split test
---
+++ batch_evaluate_random_clusters.sh
@@ -0,0 +1,1 @@
+python batch_evaluate.py --input_dir caps_files/test/ --split test --out_file ./discriminativeness_results_random_clusters.csv --random_distractors
